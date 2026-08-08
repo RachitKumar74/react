f52a0d63f1f2b0bec616675385ff03b999bf6274
Rachit Kumar <Rachitkumar3710@gmail.com>
2026-08-08 17:04:23 +0530

    01-folder

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>hello guys  </div>
-  
+
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1 id="child1">Hello</h1>
+      <h2 id="child2">Hello</h2>
+    </div>
+    <div id ="chacha"></div>
+    </> 
+  )
 }
+
 export default App;
